73264f558245adbb617654cd7887b584936e52c5
Maria Gabriella Brodi <mbrodi@pivotal.io>
2022-01-18 15:53:40 -0500

    Added Date

--- Tiltfile
+++ Tiltfile
@@ -1,6 +1,6 @@
 SOURCE_IMAGE = os.getenv("SOURCE_IMAGE", default='harbor.marygabry.name/library/tanzu-java-web-app-source')
 LOCAL_PATH = os.getenv("LOCAL_PATH", default='.')
-NAMESPACE = os.getenv("NAMESPACE", default='default')
+NAMESPACE = os.getenv("NAMESPACE", default='dev')
 
 k8s_custom_deploy(
     'tanzu-java-web-app',
@@ -20,3 +20,5 @@ k8s_custom_deploy(
 
 k8s_resource('tanzu-java-web-app', port_forwards=["8080:8080"],
             extra_pod_selectors=[{'serving.knative.dev/service': 'tanzu-java-web-app'}])
+
+allow_k8s_contexts('arn:aws:eks:us-east-2:083459807443:cluster/tap')            
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -8,7 +12,9 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		Date date = new Date(System.currentTimeMillis());
+ 		return formatter.format(date) + "Greetings from Spring Boot + VMware Tanzu to all of you!!!";
 	}
 
 }
